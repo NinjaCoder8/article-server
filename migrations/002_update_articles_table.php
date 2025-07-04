11505c880a02977636af80d31f79f2d86f888f3b
hussein alayan <alayen.hussein@gmail.com>
2025-07-04 23:36:58 +0300

    migrations: add articles and categories tables, foreignkey,  and runner script

--- migrations/002_update_articles_table.php
+++ migrations/002_update_articles_table.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 require("../connection/connection.php");
 
+$query1 = "ALTER TABLE articles ADD COLUMN category_id INT";
+$mysqli->prepare($query1)->execute();
 
-$query = "ALTER TABLE articles....";
-
-$execute = $mysqli->prepare($query);
-$execute->execute();
+$query2 = "ALTER TABLE articles ADD CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL";
+$mysqli->prepare($query2)->execute();
---
+++ migrations/runner.php
@@ -0,0 +1,5 @@
+<?php
+require("001_create_articles_table.php");
+require("003_create_categories_table.php");
+require("002_update_articles_table.php");
+echo "All migrations executed.";
